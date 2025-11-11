daa5a22bc64be3ec304107260bd9ae09767a2b0e
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2025-11-11 18:42:36 +0100

    fixed unused variable

--- code/src/main.js
+++ code/src/main.js
@@ -3,7 +3,6 @@ import './style.css'
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
-const unused = "unused";
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
